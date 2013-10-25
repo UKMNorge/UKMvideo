88ce589dfbb9eab3c7a50dd1474bc09541cf1fad
Marius Mandal <mariusmandal@gmail.com>
2013-10-25 12:23:21 +0200

    js debug

--- UKMvideo.js
+++ UKMvideo.js
@@ -24,14 +24,16 @@ function details_show( innslag ) {
 					if(!response.success) {
 						alert('Beklager, en feil oppsto ved henting av informasjon fra serveren. Vennligst prøv igjen');
 						details_hide( jQuery('#innslag_' + response.id) );
-					} else if(response.related.length == 0) {
+					} else if(response.related.length == 0 || response.related == undefined || response.related == null) {
+						console.log('Innslag '+ response.id +': Ingen video');
 						var hbt_video_ingen = Handlebars.compile( jQuery('#handlebars-innslag-video-ingen').html() );
 						jQuery('#innslag_'+response.id).find('.loader').slideUp();
 						jQuery('#innslag_'+response.id).find('.loaded').html( hbt_video_ingen( response.related ) );
 					} else {
-						var hbt_video_ingen = Handlebars.compile( jQuery('#handlebars-innslag-video-liste').html() );
+						console.log('Innslag '+ response.id +': Lag liste video');
+						var hbt_video_liste = Handlebars.compile( jQuery('#handlebars-innslag-video-liste').html() );
 						jQuery('#innslag_'+response.id).find('.loader').slideUp();
-						jQuery('#innslag_'+response.id).find('.loaded').html( hbt_video_ingen( response.related ) );						
+						jQuery('#innslag_'+response.id).find('.loaded').html( hbt_video_liste( response.related ) );
 					}
 				});
 }
