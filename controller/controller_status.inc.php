8ccef9a61a7e00eb1d4fb8a74dc8adafac55cb8d
Marius Mandal <mariusmandal@gmail.com>
2013-10-28 14:16:41 +0100

    timeout

--- controller/controller_status.inc.php
+++ controller/controller_status.inc.php
@@ -2,6 +2,7 @@
 require_once('UKM/curl.class.php');
 
 $curl_videoconverter = new UKMCURL();
+$curl_videoconverter->timeout(2);
 $status_videoconverter = $curl_videoconverter->request('http://videoconverter.ukm.no/api/status.php');
 
 var_dump($curl_videoconverter);
@@ -10,6 +11,7 @@ var_dump($status_videoconverter);
 echo 'zbhaøsokd';
 
 $curl_videoconverter = new UKMCURL();
+$curl_videoconverter->timeout(2);
 $status_videoconverter = $curl_videoconverter->request('http://videoconverterrerr.ukm.no/api/status.php');
 var_dump($curl_videoconverter);
 var_dump($status_videoconverter);
