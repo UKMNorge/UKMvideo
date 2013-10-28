ab26029dc56871935520d6094d1ed9acac4e673a
Marius Mandal <mariusmandal@gmail.com>
2013-10-28 14:18:55 +0100

    crash curl

--- controller/controller_status.inc.php
+++ controller/controller_status.inc.php
@@ -12,6 +12,6 @@ echo 'zbhaøsokd';
 
 $curl_videoconverter = new UKMCURL();
 $curl_videoconverter->timeout(2);
-$status_videoconverter = $curl_videoconverter->request('http://videoconverterrerr.ukm.no/api/status.php');
+$status_videoconverter = $curl_videoconverter->request('http://videoconverterrerr.ukm.nor/api/status.php');
 var_dump($curl_videoconverter);
 var_dump($status_videoconverter);
