03f136758af88b59780b458833aca532d08eba73
Marius Mandal <mariusmandal@gmail.com>
2013-11-05 11:54:17 +0100

    listegui

--- controller/controller_reportasje.inc.php
+++ controller/controller_reportasje.inc.php
@@ -5,13 +5,7 @@ require_once('UKM/tv_files.class.php');
 require_once('UKM/monstring.class.php');
 
 function UKMv_get_film($v_id) {
-	$sql = new SQL("SELECT *, 
-						   `video_name` AS `title`,
-						   `video_category` AS `set`
-					FROM `ukm_standalone_video` 
-					WHERE `cron_id` = '#vid'",
-				array('vid' => $v_id));
-	return $sql->run('array');
+	
 }
 
 
@@ -24,7 +18,19 @@ $sql = new SQL("SELECT `cron_id` FROM `ukm_standalone_video`
 				array('plid' => get_option('pl_id')));
 $res = $sql->run();
 while( $r = mysql_fetch_assoc( $res )) {
-	$films[] = UKMv_get_film( $r['cron_id'] );
+
+	$film = new SQL("SELECT *
+					FROM `ukm_standalone_video` 
+					WHERE `cron_id` = '#cron_id'",
+				array('cron_id' => $r['cron_id']));
+	$film = $film->run('array');
+	
+	$TV = new tv(false, $film['cron_id']);
+	
+	var_dump($TV);
+	
+	
+	$films[] = $film;
 }
 
 $INFOS['reportasjer'] = $films;
