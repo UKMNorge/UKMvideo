b3317bba4812c3ef3530d4488342f45bbe610e24
Marius Mandal <mariusmandal@gmail.com>
2013-11-05 15:01:47 +0100

    test

--- ajax/load_tv_status.ajax.php
+++ ajax/load_tv_status.ajax.php
@@ -3,8 +3,8 @@ require_once('UKM/tv.class.php');
 
 $TV = new tv(false, $_POST['cron_id']);
 
-var_dump($TV);
+//var_dump($TV);
 
 $AJAX_DATA = array('success' => true,
-				   'reload' => $TV->id !== false ? 'true' : 'false',
+				   'reload' => $TV->id !== false ? true : false,
 				   'cron_id' => $_POST['cron_id']);
