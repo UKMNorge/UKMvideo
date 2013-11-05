484c3e58c956f565857ec952caf27552e416f33e
Marius Mandal <mariusmandal@gmail.com>
2013-11-05 14:37:03 +0100

    pull-data handler

---
+++ ajax/load_tv_status.ajax.php
@@ -0,0 +1,7 @@
+<?php
+require_once('UKM/tv.class.php');
+
+$TV = new tv(false, $_POST['cron_id']);
+
+$AJAX_DATA = array('success' => true,
+				   'reload' => $TV->id !== false ? 'true' : 'false' );
