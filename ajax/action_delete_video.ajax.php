335ea83e0d945d24f8d68323e18fac243ad04f01
Marius Mandal <mariusmandal@gmail.com>
2013-11-05 14:48:37 +0100

    activate delete

--- ajax/action_delete_video.ajax.php
+++ ajax/action_delete_video.ajax.php
@@ -5,8 +5,7 @@ $TV->delete();
 
 if(isset($_POST['video_id']) && (int)$_POST['video_id'] > 0) {
 	$sqlDel = new SQLdel('ukm_standalone_video', array('v_id' => $_POST['video_id']));
-	echo $sqlDel->debug();
-//	$sqlDel->run();
+	$sqlDel->run();
 }
 
 $AJAX_DATA = array( 'success' => true,	
