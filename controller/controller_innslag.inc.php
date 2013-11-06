7157b33bc861b18b4b1ec66e6abc8ef37f495546
Marius Mandal <mariusmandal@gmail.com>
2013-11-06 11:22:26 +0100

    bugfix?

--- controller/controller_innslag.inc.php
+++ controller/controller_innslag.inc.php
@@ -44,9 +44,8 @@ if( isset( $INFOS['program'] ) ) {
 									WHERE `tv_file` LIKE '%#cronid%'
 									AND `tv_deleted` = 'true'",
 									array('cronid' => $r['cron_id']));
-				echo $sqlTest->debug();
-				$sqlTest->run();
-				if(mysql_num_rows( $sqlTest ) == 0)
+				$resTest = $sqlTest->run();
+				if(mysql_num_rows( $resTest ) == 0)
 					$coming[] = $r;
 			}	
 		}
