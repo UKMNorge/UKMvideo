81bdd08dabed60f21b655788c5a9bb9699b73f31
ukmno <ukmno@ukm.no>
2016-05-24 12:23:28 +0200

    Fil for å korrigere SMIL-status i UKM-TV

---
+++ fix_smilfiles.cron.php
@@ -0,0 +1,32 @@
+<?php
+	
+require_once('UKM/sql.class.php');
+require_once('UKM/curl.class.php');
+
+$sql = new SQL("SELECT * 
+				FROM `ukm_tv_files` 
+				WHERE `file_exists_smil` = 'unknown'
+				ORDER BY `tv_id` DESC
+				LIMIT 100
+				"
+			);
+$res = $sql->run();
+
+while( $row = mysql_fetch_assoc( $res ) ) {
+	echo '<h1>'. $row['tv_file'] .'</h1>';
+	$request = urlencode( $row['tv_file'] );
+	
+	$curl = new UKMCurl();
+	$curl->timeout( 5 );
+	$create_smil = $curl->request( 'http://video.ukm.no/create_smil.php?file='. $request );
+	
+
+	if( !$create_smil->success ) {
+		echo 'ERROR';
+	} else {
+		$ins = new SQLins('ukm_tv_files', array('tv_id' => $row['tv_id'] ) );
+		$ins->add('file_exists_smil', ($create_smil->exists ? 'true' : 'false' ));
+		echo $ins->debug();
+		$ins->run();
+	}
+}
