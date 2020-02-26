0df4be59a268fe9b4c42317041b1fce1add7eafd
Marius Mandal <mariusmandal@gmail.com>
2020-02-26 14:28:50 +0100

    bedre copy (?)

--- js/video.js
+++ js/video.js
@@ -16,12 +16,22 @@ jQuery(document).on('click', '.kopierUrl', function(e) {
     urlContainer.find('.copied').hide();
 
     urlContainer.slideDown(200, function() {
-        urlContainer.find('input').focus().select();
+        urlContainer.find('input').focus();
+        urlContainer.find('input').select();
     });
 
     if (document.execCommand("copy")) {
         urlContainer.find('input').hide();
         urlContainer.find('.copied').slideDown(120);
+
+        // thanks to: https://stackoverflow.com/a/47421284
+        var text = urlContainer.find('input').get(0); // Grab the node of the element
+        var selection = window.getSelection(); // Get the Selection object
+        var range = document.createRange(); // Create a new range
+        range.selectNodeContents(text); // Select the content of the node from line 1
+        selection.removeAllRanges(); // Delete any old ranges        
+        selection.addRange(range); // Add the range to selection
+        document.execCommand('copy'); // Execute the command
 
         setTimeout(
             function() {
