f809232c74b6477b099031f39410f40b0dc2cc50
Kushtrim Aliu <kushtrimaliu19@gmail.com>
2021-04-06 17:35:15 +0200

    Revert "Statisk path for resurser"
    This reverts commit e72ae89ff03bcd1c64ddc6c120279f37d02d8362.

--- ukmvideo.php
+++ ukmvideo.php
@@ -106,41 +106,41 @@ class UKMvideo extends Modul
 
         wp_enqueue_script('WPbootstrap3_js');
         wp_enqueue_style('WPbootstrap3_css');
-        wp_enqueue_style( 'UKMvideo_css', PLUGIN_PATH . 'UKMvideo/UKMvideo.css');
-        wp_enqueue_script( 'UKMvideo_js', PLUGIN_PATH . 'UKMvideo/js/video.js');
+        wp_enqueue_style( 'UKMvideo_css', static::getPluginUrl() . 'UKMvideo.css');
+        wp_enqueue_script( 'UKMvideo_js', static::getPluginUrl() . 'js/video.js');
         if( isset($_GET['action'] ) && $_GET['action'] == 'flerkamera' ) {
-            wp_enqueue_script( 'UKMvideo_js_upload_innslag', PLUGIN_PATH . 'UKMvideo/js/upload_innslag.js');
+            wp_enqueue_script( 'UKMvideo_js_upload_innslag', static::getPluginUrl() . 'js/upload_innslag.js');
         } else {
-            wp_enqueue_script( 'UKMvideo_js_upload', PLUGIN_PATH . 'UKMvideo/js/upload.js');
+            wp_enqueue_script( 'UKMvideo_js_upload', static::getPluginUrl() . 'js/upload.js');
         }
         
         wp_enqueue_script('jquery');
         wp_enqueue_script('jqueryGoogleUI', '//ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/jquery-ui.min.js');
     
-        wp_enqueue_style( 'blueimp-gallery-css', PLUGIN_PATH . 'UKMvideo/jqueryuploader/css/blueimp-gallery.min.css');
+        wp_enqueue_style( 'blueimp-gallery-css', static::getPluginUrl() . 'jqueryuploader/css/blueimp-gallery.min.css');
     
         // CSS to style the file input field as button and adjust the Bootstrap progress bars
-        wp_enqueue_style( 'jquery-fileupload-css', PLUGIN_PATH . 'UKMvideo/jqueryuploader/css/jquery.fileupload.css');
-        wp_enqueue_style( 'jquery-fileupload-ui-css', PLUGIN_PATH . 'UKMvideo/jqueryuploader/css/jquery.fileupload-ui.css');
+        wp_enqueue_style( 'jquery-fileupload-css', static::getPluginUrl() . 'jqueryuploader/css/jquery.fileupload.css');
+        wp_enqueue_style( 'jquery-fileupload-ui-css', static::getPluginUrl() . 'jqueryuploader/css/jquery.fileupload-ui.css');
         
         // The jQuery UI widget factory, can be omitted if jQuery UI is already included
-        wp_enqueue_script('jquery_ui_widget', PLUGIN_PATH . 'UKMvideo/jqueryuploader/js/vendor/jquery.ui.widget.js');
+        wp_enqueue_script('jquery_ui_widget', static::getPluginUrl() . 'jqueryuploader/js/vendor/jquery.ui.widget.js');
         // The Load Image plugin is included for the preview images and image resizing functionality
-        wp_enqueue_script('load-image', PLUGIN_PATH . 'UKMvideo/jqueryuploader/js/vendor/load-image.min.js');
+        wp_enqueue_script('load-image', static::getPluginUrl() . 'jqueryuploader/js/vendor/load-image.min.js');
         // The Canvas to Blob plugin is included for image resizing functionality
-        wp_enqueue_script('canvas-to-blob', PLUGIN_PATH . 'UKMvideo/jqueryuploader/js/vendor/canvas-to-blob.min.js');
+        wp_enqueue_script('canvas-to-blob', static::getPluginUrl() . 'jqueryuploader/js/vendor/canvas-to-blob.min.js');
         // The Iframe Transport is required for browsers without support for XHR file uploads
-        wp_enqueue_script('iframe-transport', PLUGIN_PATH . 'UKMvideo/jqueryuploader/js/jquery.iframe-transport.js');	
+        wp_enqueue_script('iframe-transport', static::getPluginUrl() . 'jqueryuploader/js/jquery.iframe-transport.js');	
         // The basic File Upload plugin
-        wp_enqueue_script('fileupload', PLUGIN_PATH . 'UKMvideo/jqueryuploader/js/jquery.fileupload.js');	
+        wp_enqueue_script('fileupload', static::getPluginUrl() . 'jqueryuploader/js/jquery.fileupload.js');	
         // The File Upload user interface plugin
-        wp_enqueue_script('fileupload-ui', PLUGIN_PATH . 'UKMvideo/jqueryuploader/js/jquery.fileupload-ui.js');
+        wp_enqueue_script('fileupload-ui', static::getPluginUrl() . 'jqueryuploader/js/jquery.fileupload-ui.js');
         // The File Upload processing plugin
-        wp_enqueue_script('fileupload-process', PLUGIN_PATH . 'UKMvideo/jqueryuploader/js/jquery.fileupload-process.js');	
+        wp_enqueue_script('fileupload-process', static::getPluginUrl() . 'jqueryuploader/js/jquery.fileupload-process.js');	
         // The File Upload image preview & resize plugin 
-        wp_enqueue_script('fileupload-image', PLUGIN_PATH . 'UKMvideo/jqueryuploader/js/jquery.fileupload-image.js');	
+        wp_enqueue_script('fileupload-image', static::getPluginUrl() . 'jqueryuploader/js/jquery.fileupload-image.js');	
         // The File Upload validation plugin
-        wp_enqueue_script('fileupload-validate', PLUGIN_PATH . 'UKMvideo/jqueryuploader/js/jquery.fileupload-validate.js');	
+        wp_enqueue_script('fileupload-validate', static::getPluginUrl() . 'jqueryuploader/js/jquery.fileupload-validate.js');	
     }
 
 }
