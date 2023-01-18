409af20af9dca0745d2c0f864aa83d83dae1343c
Kushtrim Aliu <kushtrimaliu19@gmail.com>
2023-01-18 21:21:01 +0100

    Fjernet Director for nå

--- src/index.ts
+++ src/index.ts
@@ -1,83 +1,83 @@
-import Vue from "vue";
-import { SPAInteraction } from 'ukm-spa/SPAInteraction';
+// import Vue from "vue";
+// import { SPAInteraction } from 'ukm-spa/SPAInteraction';
 import {uploadVideoTabs} from './uploadVideoTabs';
-import { Director } from 'ukm-spa/Director';
+// import { Director } from 'ukm-spa/Director';
 
 
-var director = new Director();
-(<any>window).director = director;
+// var director = new Director();
+// (<any>window).director = director;
 
-var page = director.getParam('pageSPA');
+// var page = director.getParam('pageSPA');
 
-console.log(page);
+// console.log(page);
 
-if(!page) {
-    page = 'upload';
-}
+// if(!page) {
+//     page = 'upload';
+// }
 
 
 
-director.addEventListener('openPage', function(obj : any) {
-    if(obj.id == 'upload') {
+// director.addEventListener('openPage', function(obj : any) {
+//     if(obj.id == 'upload') {
         uploadVideoTabs();
-    }
-    else {
-        alert('Unavailable id');
-    }
+//     }
+//     else {
+//         alert('Unavailable id');
+//     }
     
-    // console.log('aaa');
-    // jQuery(".menu-items button.menu-item").removeClass('active');
-    // jQuery(".menu-item."+ obj.id).addClass('active');
-    // console.log(".menu-item."+ obj.id);
-})
+//     // console.log('aaa');
+//     // jQuery(".menu-items button.menu-item").removeClass('active');
+//     // jQuery(".menu-item."+ obj.id).addClass('active');
+//     // console.log(".menu-item."+ obj.id);
+// })
 
-director.openPage(page);
+// director.openPage(page);
 
 
-// declare var ajaxurl: string; // Kommer fra global
-// let v = new Vue({
-//     el: "#videoVueApp",
-//     data: { 
-//         name: "World",
-//         activeTab : 'deltabruk',
-//         spaInteraction: new SPAInteraction(null, 'https://ukm.dev/wp-admin/admin-ajax.php')
-//     },
+// // declare var ajaxurl: string; // Kommer fra global
+// // let v = new Vue({
+// //     el: "#videoVueApp",
+// //     data: { 
+// //         name: "World",
+// //         activeTab : 'deltabruk',
+// //         spaInteraction: new SPAInteraction(null, 'https://ukm.dev/wp-admin/admin-ajax.php')
+// //     },
     
-//     components: {
+// //     components: {
 
-//     },
+// //     },
 
-//     mounted : function() {
-//         this.testAjax();
-//     },
+// //     mounted : function() {
+// //         this.testAjax();
+// //     },
 
-//     methods : {
-//         testAjax : function() {
-//             var data = {
-//                 action: 'UKMvideo_ajax',
-//                 subaction: 'text',
-//             };
+// //     methods : {
+// //         testAjax : function() {
+// //             var data = {
+// //                 action: 'UKMvideo_ajax',
+// //                 subaction: 'text',
+// //             };
             
-//             console.log(ajaxurl);
+// //             console.log(ajaxurl);
     
-//             var response = this.spaInteraction.runAjaxCall('/', 'POST', data);
+// //             var response = this.spaInteraction.runAjaxCall('/', 'POST', data);
 
-//             console.log(response);
+// //             console.log(response);
             
-//         },
-//         // Open tab
-//         velgKomponent: function() {
+// //         },
+// //         // Open tab
+// //         velgKomponent: function() {
 
-//         }
-//     },
+// //         }
+// //     },
 
-//     template: /*html*/`
-//     <div>    
-//         <div>
-//             <h1>Here is the value {{name}}</h1>
-//         </div>
+// //     template: /*html*/`
+// //     <div>    
+// //         <div>
+// //             <h1>Here is the value {{name}}</h1>
+// //         </div>
 
-//     </div>
+// //     </div>
 
-//     `
-// });
+// //     `
+// // });
