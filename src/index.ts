cd09a39b42245645c07e265ba6ebb6183cfcc400
Kushtrim Aliu <kushtrimaliu19@gmail.com>
2023-02-23 16:51:09 +0100

    Fjerner gamle kode og legger til tab argument
    Legger til tab argument for å bruke det på identifisering av tabs

--- src/index.ts
+++ src/index.ts
@@ -1,83 +1,15 @@
-// import Vue from "vue";
-// import { SPAInteraction } from 'ukm-spa/SPAInteraction';
+import { SPAInteraction } from 'ukm-spa/SPAInteraction';
 import {uploadVideoTabs} from './uploadVideoTabs';
-// import { Director } from 'ukm-spa/Director';
+import { Director } from 'ukm-spa/Director';
 
+var director = new Director();
+(<any>window).director = director;
 
-// var director = new Director();
-// (<any>window).director = director;
+var activeTab = director.getParam('tabSPA');
 
-// var page = director.getParam('pageSPA');
+// Hvis tab finnes ikke, set det t
+if(!activeTab) {
+    activeTab = 'reportasje';
+}
 
-// console.log(page);
-
-// if(!page) {
-//     page = 'upload';
-// }
-
-
-
-// director.addEventListener('openPage', function(obj : any) {
-//     if(obj.id == 'upload') {
-        uploadVideoTabs();
-//     }
-//     else {
-//         alert('Unavailable id');
-//     }
-    
-//     // console.log('aaa');
-//     // jQuery(".menu-items button.menu-item").removeClass('active');
-//     // jQuery(".menu-item."+ obj.id).addClass('active');
-//     // console.log(".menu-item."+ obj.id);
-// })
-
-// director.openPage(page);
-
-
-// // declare var ajaxurl: string; // Kommer fra global
-// // let v = new Vue({
-// //     el: "#videoVueApp",
-// //     data: { 
-// //         name: "World",
-// //         activeTab : 'deltabruk',
-// //         spaInteraction: new SPAInteraction(null, 'https://ukm.dev/wp-admin/admin-ajax.php')
-// //     },
-    
-// //     components: {
-
-// //     },
-
-// //     mounted : function() {
-// //         this.testAjax();
-// //     },
-
-// //     methods : {
-// //         testAjax : function() {
-// //             var data = {
-// //                 action: 'UKMvideo_ajax',
-// //                 subaction: 'text',
-// //             };
-            
-// //             console.log(ajaxurl);
-    
-// //             var response = this.spaInteraction.runAjaxCall('/', 'POST', data);
-
-// //             console.log(response);
-            
-// //         },
-// //         // Open tab
-// //         velgKomponent: function() {
-
-// //         }
-// //     },
-
-// //     template: /*html*/`
-// //     <div>    
-// //         <div>
-// //             <h1>Here is the value {{name}}</h1>
-// //         </div>
-
-// //     </div>
-
-// //     `
-// // });
+uploadVideoTabs(activeTab);
