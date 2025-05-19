8b9acc0e47079880cd8bc7d42f76583eef6aff4d
Kushtrim Aliu <kushtrimaliu19@gmail.com>
2025-05-19 16:09:32 +0200

    Ikke sendt fix 2

--- src/objects/InnslagVideo.ts
+++ src/objects/InnslagVideo.ts
@@ -108,8 +108,10 @@ export default class InnslagVideo {
             if(samtykke.status.id != 'godkjent') {
                 samtykkerNei = true;
             }
-            if(samtykke.foresatt && samtykke.foresatt.status.id != 'ikke_sendt' && samtykke.foresatt.status.id != 'godkjent') {
-                samtykkerNei = true;
+            if(samtykke.foresatt && samtykke.foresatt.status.id != 'ikke_sendt') {
+                if(samtykke.foresatt.status.id != 'godkjent') {
+                    samtykkerNei = true;
+                }
             }
             
             if(samtykkerNei) countNei++;
